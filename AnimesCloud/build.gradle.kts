e0fe273d554514bf001100b69dab8b19b0f64ffc
euluan1912 <euluan1912@gmail.com>
2025-11-29 06:26:09 -0300

    Atualizar o build.gradle.kts

--- AnimesCloud/build.gradle.kts
+++ AnimesCloud/build.gradle.kts
@@ -3,7 +3,7 @@ version = 1
 cloudstream {
     description = "AnimesCloud - Animes em FHD e HD"
     language = "pt-br"
-    authors = listOf("saimuelbr")
+    authors = listOf("euluan1912")
     status = 1
     tvTypes = listOf("Anime", "AnimeMovie")
     iconUrl = "https://animesonline.cloud/wp-content/uploads/2025/06/logo-_1_.png"
