b14f6eef51f3ae09c036449d42cb886193d42394
euluan1912 <euluan1912@gmail.com>
2025-12-04 06:10:44 -0300

    Atualizar o UltraCinePlugin.kt

--- UltraCine/src/main/kotlin/com/UltraCine/UltraCinePlugin.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/UltraCinePlugin.kt
@@ -1,7 +1,7 @@
 package com.UltraCine
 
-import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.BasePlugin
 
 @CloudstreamPlugin
 class UltraCineProvider : BasePlugin() {
