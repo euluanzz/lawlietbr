e2db590857d66e2ae896a782993a7c5819efdf01
L <euluan1912@gmail.com>
2025-12-06 10:51:21 -0300

    Atualizar o EmbedPlayExtractor.kt

--- UltraCine/src/main/kotlin/com/UltraCine/EmbedPlayExtractor.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/EmbedPlayExtractor.kt
@@ -2,20 +2,12 @@ package com.UltraCine
 
 import com.lagradost.cloudstream3.extractors.VidStack
 
-// CORRIGIDO: Este deve cobrir o link 'embedplay.upns.ink' que você viu no site.
-class EmbedPlayUpnsInk : VidStack() {
-    override var name = "EmbedPlay Upns Ink"
-    override var mainUrl = "http://embedplay.upns.ink" // Use http ou https
-}
-
-// Mantenha este, caso o UltraCine volte a usar o .pro
 class EmbedPlayUpnsPro : VidStack() {
     override var name = "EmbedPlay UpnsPro"
-    override var mainUrl = "https://embedplay.upns.pro" 
+    override var mainUrl = "https://embedplay.upns.pro"
 }
 
-// Mantenha este, caso o UltraCine use o .one
 class EmbedPlayUpnOne : VidStack() {
     override var name = "EmbedPlay UpnOne"
-    override var mainUrl = "http://embedplay.upn.one"
+    override var mainUrl = "https://embedplay.upn.one"
 }
