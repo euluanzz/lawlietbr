ba60205ac159b941841b38522bd6a5fa9e675e39
L <euluan1912@gmail.com>
2026-01-19 12:24:13 -0300

    Add CineAgoraPlugin class for Cloudstream integration
    Signed-off-by: L <euluan1912@gmail.com>

---
+++ CineAgora/src/main/kotlin/com/CineAgora/CineAgoraPlugin.kt
@@ -0,0 +1,12 @@
+package com.CineAgora
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class CineAgoraPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(CineAgora())
+    }
+}
