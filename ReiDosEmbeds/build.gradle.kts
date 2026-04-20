f2940b14372c84f21ef8e0fbf8b41e767f15cfce
L <euluan1912@gmail.com>
2026-04-20 11:38:10 -0300

    Rei Dos Embeds V1
    Configuração inicial do plugin.
    
    Signed-off-by: L <euluan1912@gmail.com>

---
+++ ReiDosEmbeds/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Rei dos Embeds - O melhor diretório de canais ao vivo. Mais de 150 canais disponíveis."
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.image2url.com/r2/default/images/1776423716151-20e5aa74-a764-4d60-8575-0851d8ab37df.png"
+    isCrossPlatform = true
+}
