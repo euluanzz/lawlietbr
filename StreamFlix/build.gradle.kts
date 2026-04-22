44b13acb3983c388717f8986a43fe184e9d68e2a
L <euluan1912@gmail.com>
2026-04-22 14:12:34 -0300

    StreamFlix V1
    Build do novo plugin.
    
    Signed-off-by: L <euluan1912@gmail.com>

---
+++ StreamFlix/build.gradle.kts
@@ -0,0 +1,55 @@
+import java.util.Properties
+
+plugins {
+    id("com.android.library")
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+    
+    namespace = "com.StreamFlix"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        val tmdbApiKey = project.findProperty("TMDB_API_KEY") as? String
+            ?: System.getenv("TMDB_API_KEY")
+            ?: getLocalProperty("TMDB_API_KEY")
+            ?: "dummy_api_key"
+
+        val tmdbAccessToken = project.findProperty("TMDB_ACCESS_TOKEN") as? String
+            ?: System.getenv("TMDB_ACCESS_TOKEN")
+            ?: getLocalProperty("TMDB_ACCESS_TOKEN")
+            ?: "dummy_access_token"
+
+        buildConfigField("String", "TMDB_API_KEY", "\"$tmdbApiKey\"")
+        buildConfigField("String", "TMDB_ACCESS_TOKEN", "\"$tmdbAccessToken\"")
+    }
+}
+
+fun getLocalProperty(key: String): String? {
+    val localProperties = Properties()
+    val localPropertiesFile = project.rootProject.file("local.properties")
+    return if (localPropertiesFile.exists()) {
+        localProperties.load(localPropertiesFile.inputStream())
+        localProperties.getProperty(key)
+    } else null
+}
+
+cloudstream {
+    version = 1
+    description = "StreamFlix - Filmes e Séries Online em Português | Lançamentos e Clássicos"
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Movies", "Series")
+    iconUrl = "https://openclipart.org/image/2400px/svg_to_png/193323/-S.png"
+}
+
+dependencies {
+    val cloudstream by configurations
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+}
