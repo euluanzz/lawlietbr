fd1ff2ecc35ebc824010f9e8cf01ff6ea80b94c3
guilhermehanti <guilhermehanti@gmail.com>
2026-07-31 16:23:44 -0300

    up

--- DattebayoBR/build.gradle.kts
+++ DattebayoBR/build.gradle.kts
@@ -1,10 +1,10 @@
-version = 2
+version = 1
 
 cloudstream {
     description = "DattebayoBR Animes Online"
     language = "pt-br"
     authors = listOf("lawlietbr")
     status = 1
-    tvTypes = listOf("Anime", "AsianDrama")
+    tvTypes = listOf("Anime, AsianDrama")
     iconUrl = "https://www.dattebayo-br.com/favicon.png"
 }
